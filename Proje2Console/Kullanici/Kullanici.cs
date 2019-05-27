1cc140243ff4db19e2b318231583defcdba31415
Taylan Unutmaz <taylanunutmaz@gmail.com>
2019-05-27 22:36:39 +0300

    Upload

---
+++ Proje2Console/Kullanici/Kullanici.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proje2Console
+{
+    abstract class Kullanici
+    {
+        internal string ID { get; }
+        string Ad { get; }
+        string Soyad { get; }
+        string TelefonNumarasi { get; }
+        
+    }
+}
