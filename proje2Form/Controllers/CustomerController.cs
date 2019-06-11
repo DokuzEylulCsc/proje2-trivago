9ab5513e3705d2b43ee744059d8820df4aa3082c
Taylan Unutmaz <taylanunutmaz@gmail.com>
2019-06-11 12:46:48 +0300

    CustomerController ve HotelController oluşturuldu

---
+++ proje2Form/Controllers/CustomerController.cs
@@ -0,0 +1,24 @@
+﻿using proje2Form.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proje2Form.Controllers
+{
+    class CustomerController
+    {
+        List<Customer> customers = new List<Customer>();
+        int lastid = 1000;
+
+        public void CreateCustomer(string name, string surname)
+        {
+            Customer customer = new Customer();
+            customer.Id = lastid++;
+            customer.Name = name;
+            customer.Surname = surname;
+            customers.Add(customer);
+        }
+    }
+}
---
+++ proje2Form/Controllers/HotelController.cs
@@ -0,0 +1,20 @@
+﻿using proje2Form.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace proje2Form.Controllers
+{
+    class HotelController
+    {
+        public void CreateMotel(int star, string name)
+        {
+            Motel motel = new Motel();
+            motel.Star = star;
+            motel.Name = name;
+            //TODO Hoteller bir yerde tutulmalı
+        }
+    }
+}
